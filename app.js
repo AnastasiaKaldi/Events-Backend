5860dbcb5ddb3913648836dfeae92cd4e9b3cdea
AnastasiaKaldi <kaldianastasia@gmail.com>
2025-06-06 20:56:07 +0100

    Test Netlify 2

--- app.js
+++ app.js
@@ -15,21 +15,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(
-  cors({
-    origin: "https://eventino.netlify.app",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-  })
-);
+const corsOptions = {
+  origin: "https://eventino.netlify.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+};
 
-app.use((req, res, next) => {
-  if (ENV === "production") {
-    res.header("Access-Control-Allow-Credentials", "true");
-  }
-  next();
-});
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
